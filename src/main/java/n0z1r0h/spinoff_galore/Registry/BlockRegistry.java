d112dbdff17d23eccc3818e3be1947444a8afadd
N0z1r0h <ntge31@gmail.com>
2025-08-06 19:34:48 -0400

    Create BlockRegistry.java

---
+++ src/main/java/n0z1r0h/spinoff_galore/Registry/BlockRegistry.java
@@ -0,0 +1,27 @@
+package n0z1r0h.spinoff_galore.Registry;
+
+import n0z1r0h.spinoff_galore.SpinoffsGalore;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class BlockRegistry {
+    public static Block register(String name, Function<AbstractBlock.Settings, Block> blockFactory, AbstractBlock.Settings settings, boolean shouldRegisterItem) {
+        RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(SpinoffsGalore.MOD_ID, name));
+        Block block = blockFactory.apply(settings.registryKey(blockKey));
+        if(shouldRegisterItem) {
+            RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SpinoffsGalore.MOD_ID, name));
+            BlockItem blockItem = new BlockItem(block, new Item.Settings().registryKey(itemKey));
+            Registry.register(Registries.ITEM, itemKey, blockItem);
+        }
+        return Registry.register(Registries.BLOCK, blockKey, block);
+    }
+}
