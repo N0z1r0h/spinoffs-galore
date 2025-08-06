d8d86659ae91a46e7282d64383eb5d5770a12587
N0z1r0h <ntge31@gmail.com>
2025-08-06 19:24:33 -0400

    initialize project

---
+++ src/client/java/n0z1r0h/spinoff_galore/SpinoffsGaloreClient.java
@@ -0,0 +1,10 @@
+package n0z1r0h.spinoff_galore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpinoffsGaloreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
