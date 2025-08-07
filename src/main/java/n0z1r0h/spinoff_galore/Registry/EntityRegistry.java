fa9c243878f7f693202936bd9b5ef20a8177e18a
N0z1r0h <ntge31@gmail.com>
2025-08-06 20:06:26 -0400

    Create EntityRegistry.java

---
+++ src/main/java/n0z1r0h/spinoff_galore/Registry/EntityRegistry.java
@@ -0,0 +1,17 @@
+package n0z1r0h.spinoff_galore.Registry;
+
+import n0z1r0h.spinoff_galore.SpinoffsGalore;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class EntityRegistry {
+    private static <T extends Entity> EntityType<T> register(String name, EntityType.Builder<T> builder) {
+        RegistryKey<EntityType<?>> key = RegistryKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(SpinoffsGalore.MOD_ID, name));
+        return Registry.register(Registries.ENTITY_TYPE, key, builder.build(key));
+    }
+}
